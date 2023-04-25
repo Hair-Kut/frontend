af22f9c7faddb7ef678de46f4fa821b3c6d76306
cotton <bmtosss@gmail.com>
2023-04-25 10:33:33 +0900

    feat: babel, webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+module.exports = {
+  entry: './src/index.tsx',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval' : 'hidden-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: '> 0.25%, not dead',
+                debug: true,
+              },
+            ],
+            ['@babel/preset-react', { runtime: 'automatic' }],
+            '@babel/preset-typescript',
+          ],
+        },
+        exclude: path.join(__dirname, 'node_modules'),
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'dist/[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+    new CleanWebpackPlugin(),
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+  ],
+  devServer: {
+    hot: true,
+    port: 3000,
+  },
+};
