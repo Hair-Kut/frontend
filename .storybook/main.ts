289f4560d6b17c03bcc0cdf8cb9a982d353301c4
cotton <bmtosss@gmail.com>
2023-04-27 14:43:11 +0900

    chore: storybook path 설정
    - public path
    - path alias

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -12,6 +14,14 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
+  webpackFinal: async (config: any) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      src: path.resolve(__dirname, '../src/'),
+    };
+    return config;
   },
 };
 export default config;
