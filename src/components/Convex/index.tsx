3912440349c8278458c80e5757b511ac9b5cc6c0
cotton <bmtosss@gmail.com>
2023-05-24 17:47:03 +0900

    refact: condition props 추가

--- src/components/Convex/index.tsx
+++ src/components/Convex/index.tsx
@@ -2,8 +2,12 @@ import { PropsWithChildren } from 'react';
 
 import styles from './style.module.css';
 
-function Convex({ children }: PropsWithChildren) {
-  return <div className={styles['convex-wrapper']}>{children}</div>;
+interface Props extends PropsWithChildren {
+  condition?: boolean;
+}
+
+function Convex({ children, condition = true }: Props) {
+  return <div className={`${condition && styles['convex-wrapper']}`}>{children}</div>;
 }
 
 export default Convex;
