5fda77c513c1e8a0c4de7f753c779bd02377e1fa
cotton <bmtosss@gmail.com>
2023-05-29 22:44:56 +0900

    chore: setInterval 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import HomePage from 'src/pages/Home';
 
 import '../public/img/hair-kut.svg';
-import '../public/img/hair1.svg';
-import '../public/img/hair2.svg';
-import '../public/img/hair3.svg';
-import '../public/img/hair4.svg';
-import '../public/img/hair5.svg';
-import '../public/img/hair6.svg';
-import '../public/img/hair7.svg';
-import '../public/img/hair8.svg';
-import '../public/img/hair9.svg';
-import '../public/img/hair10.svg';
-import '../public/img/hair11.svg';
+import '../public/img/hair1.png';
+import '../public/img/hair2.png';
+import '../public/img/hair3.png';
+import '../public/img/hair4.png';
+import '../public/img/hair5.png';
 import '../public/img/image.png';
 import '../public/img/logo.png';
 import '../public/img/result.png';
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
 import ImageUploader from 'src/components/ImageUploader';
@@ -6,11 +6,13 @@ import HairList from 'src/components/HairList';
 import ResultImage from 'src/components/ResultImage';
 import Logo from 'src/components/Logo/index';
 import Button from 'src/components/Button';
+import Loading from 'src/components/Loading';
 
 import { ImageSrcContext } from 'src/contexts/imageSrc';
 
 import usePresignedURL from 'src/hooks/usePresignedUrl';
 import useImageUpload from 'src/hooks/useImageUpload';
+import useInferenceCheck from 'src/hooks/useInferenceCheck';
 
 import { SERVER_URL } from 'src/constants';
 
@@ -19,15 +21,18 @@ import styles from './style.module.css';
 function HomePage() {
   const [resultImageSrc, setResultImageSrc] = useState('');
   const [selectedHair, setSelectedHair] = useState(-1);
+  const [start, setStart] = useState(false);
   const { id, presignedUrl } = usePresignedURL();
   const {
     imageSrc: userImageSrc,
     isImageUploaded,
     handleUpload,
   } = useImageUpload(id, presignedUrl);
+  const isInferenceFinished = useInferenceCheck(start, id);
 
   const handleClick = async () => {
     await axios.post(SERVER_URL + `/api/memberImages/upload_complete?id=${id}`);
+    setStart(true);
   };
 
   return (
@@ -40,6 +45,7 @@ function HomePage() {
         selectedHair,
         setSelectedHair,
       }}>
+      {start && !isInferenceFinished && <Loading />}
       <div className={styles['home-wrapper']}>
         <Logo />
         <div className={styles['home-section']}>
